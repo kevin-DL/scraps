9b8222fc9b6debd5f465c80841854e74cd9426aa
kevin-DL <kevanatole@gmail.com>
2024-06-27 20:21:14 +0100

    Change ranking mode on search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -21,7 +21,7 @@ class SearchController extends Controller
         $connector = new SpoonacularConnector();
         $request = new FindRecipesByIngredients(
             ingredients: array_map("trim", explode(",", $data['ingredients'])),
-            ranking: 2
+            ranking: 1
         );
 
         $response = $connector->send($request);
