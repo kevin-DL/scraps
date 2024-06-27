203799d2d49a03777b5574e62bc4ed2d1f04b965
kevin-DL <kevanatole@gmail.com>
2024-06-27 19:49:44 +0100

    Add logging on search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Integrations\Spoonacular\Requests\FindRecipesByIngredients;
 use App\Http\Integrations\Spoonacular\SpoonacularConnector;
 use App\Http\Requests\RecipeSearchRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class SearchController extends Controller
@@ -12,6 +14,10 @@ class SearchController extends Controller
     public function recipes(RecipeSearchRequest $request)
     {
         $data = $request->validated();
+        Log::info('Search request', [
+            'user' => Auth::user()->id,
+            'data' => $data,
+        ]);
         $connector = new SpoonacularConnector();
         $request = new FindRecipesByIngredients(
             ingredients: array_map("trim", explode(",", $data['ingredients'])),
